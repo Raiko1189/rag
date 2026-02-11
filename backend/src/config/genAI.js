7470e302460551649069a00c48661f05e95589ec
Reiko <hirkowak123@gmail.com>
2026-02-11 01:12:35 -0800

    rag is retriving the data correctly

--- backend/src/config/genAI.js
+++ backend/src/config/genAI.js
@@ -1,5 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const apiKey = process.env.GEMINI_API_KEY;
--- backend/src/route.js
+++ backend/src/route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import uploadDataRoute from "./module/uploadData/upload.route.js"
+import ragRoute from "./module/rag/rag.route.js"
 
 const router = Router()
 
 router.use("/upload", uploadDataRoute)
+router.use("/query", ragRoute)
 
 export default router
